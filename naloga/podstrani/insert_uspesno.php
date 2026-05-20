ee65dee3800b85fddfff0380d1221845de5449ab
Sers2026DominikPletersek <dominik.pletersek@dijaki.sers.si>
2026-05-20 18:07:50 +0200

    Delete naloga directory

--- naloga/podstrani/insert_uspesno.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Podatek uspešno vstavljen v bazo";
-?>
--- naloga/podstrani/povezava.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "easistent";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-mysqli_set_charset($conn,"utf8mb4");
-
-?>
